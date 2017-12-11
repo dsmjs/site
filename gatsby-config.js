c8bc30f6216888ee6dd386680942226c8436ffd1
Matt Travi <programmer@travi.org>
2017-12-11 01:22:20 -0600

    build(glamor): configured build details for glamor styles
    while the styles were showing up in develop mode, they were being lost in the static build

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `dsmJS  - Des Moines JavaScript User Group`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-glamor'
+  ]
+};
