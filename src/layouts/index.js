b65c343f6a56c8bd2f9eb19e6ce53a8a8c5c5f9a
Matt Travi <programmer@travi.org>
2017-12-13 21:51:25 -0600

    feat(page-title): configured helmet in the layout component to handle titles
    page titles can now be set at the component level

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import Helmet from 'react-helmet'
 import {Layout} from '@dsmjs/components';
+import {siteMetadata} from '../../gatsby-config'
 
 export default function ({children}) {
-  return <Layout sponsor="foo" location="bar">{children()}</Layout>
+  return (
+    <Layout sponsor="foo" location="bar">
+      <Helmet titleTemplate={`%s | ${siteMetadata.title}`} defaultTitle={siteMetadata.title} />
+      {children()}
+    </Layout>
+  );
 };
